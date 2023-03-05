b474361c239984cec7551fe3ba4b447666cc45db
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-03-04 19:30:11 -0500

    adding

--- react/src/pages/index.js
+++ react/src/pages/index.js
@@ -13,7 +13,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <main></main>
+      <main>
+        <section className='bg-blue-700 h-screen  '></section>
+      </main>
       </div>
                
           
